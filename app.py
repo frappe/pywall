898ac201a96d89c6241f33ab35442394c68cdda1
Pratik Vyas <m@pd.io>
2014-09-20 23:46:35 +0530

    tangle app.py from org file

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
+
 import os
-import MySQLdb
-import datetime
-
-from flask import Flask, request, redirect, render_template
-
-from jinja2 import Environment, FileSystemLoader
-
-app = Flask(__name__)
-
 conf = {
 	'db': os.environ.get('PYWALL_DB', 'pywall'),
 	'dbhost': os.environ.get('PYWALL_DBHOST', 'localhost'),
@@ -15,13 +7,13 @@ conf = {
 	'dbpass': os.environ.get('PYWALL_DBPASS', 'pywall'),
 }
 
-# ORM
- 
+import MySQLdb
 def sql(query):
 	"""
-		executes an sql query
+	executes an sql query
 	"""
-	conn = MySQLdb.connect(conf['dbhost'], conf['dbuser'], conf['dbpass'])
+	conn = MySQLdb.connect(conf['dbhost'], conf['dbuser'], 
+			       conf['dbpass'])
 	conn.select_db(conf['db'])
 	conn.autocommit(True)
 	cursor = conn.cursor()
@@ -32,27 +24,38 @@ def sql(query):
 
 def insert_record(table, record):
 	"""
-		Insert a record(dict) in a table
+	Insert a record(dict) in a table
 	"""
-	columns = ",".join(record.keys())
-	values = "'" + "', '".join(record.values()) + "'"
-	query = "insert into `{}` ({}) values({})".format(table, columns, values)
+	columns = record.keys()
+	values = record.values()
+
+	# ['col1', 'col2', 'col3' ] -> "col1,col2,col3"
+	columns_fragment = ",".join(columns)
+
+	# ['val1', 'val2', 'val3' ] to "'val1', 'val2', 'val3'"
+	values_fragment = "'" + "', '".join(values) + "'"
+
+	query = "insert into `{}` ({}) values({})".format(table, 
+				columns_fragment, values_fragment)
 	sql(query)
 
 def list_records(table, columns, order_by=None):
 	"""
-		Get all records (with list of columns) of a table.
+	Get all records (with list of columns) of a table.
 	"""
-	order_by = "order by {}".format(order_by)
-	query = "select {} from {} {}".format(', '.join(columns), table, order_by)
-	values = sql(query)
-	return [dict(zip(columns, value)) for value in values]
+	ret = []
+	order_by_fragment = "order by {}".format(order_by) if order_by else ""
+	query = "select {} from {} {}".format(', '.join(columns), 
+				table, order_by_fragment)
+	rows = sql(query)
+	for row in rows:
+		ret.append(dict(zip(columns, row)))
+	return ret
 
-# Wall methods
-
+import datetime
 def add_to_wall(message, posted_by):
 	"""
-		Add a message to the wall
+	Add a message to the wall
 	"""
 	insert_record('wall', {
 		'message': message,
@@ -62,11 +65,13 @@ def add_to_wall(message, posted_by):
 
 def get_wall():
 	"""
-		Get all messages on wall
+	Get all messages on wall
 	"""
-	return list_records('wall', ['message', 'posted_by', 'posted_on'], order_by='-posted_on')
+	return list_records('wall', ['message', 'posted_by', 'posted_on'], 
+			    order_by='-posted_on')
 
-# Web views
+from flask import Flask, request, redirect, render_template
+app = Flask(__name__)
 
 @app.route('/')
 def get_wall_view():
@@ -79,5 +84,5 @@ def add_to_wall_view():
 	add_to_wall(message, posted_by)
 	return redirect('/')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	app.run(debug=True)
